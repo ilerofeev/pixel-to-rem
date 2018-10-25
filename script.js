f592114de74a82681c7758e32562d94aa5b9b783
Ilya Erofeev <nestle3195@yandex.ru>
2018-10-25 23:35:36 +0300

    final version

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 new Vue({
 	el: '#app',
 	data: {
-		fontSize: 14,
-		valuePx: 100
+		fontSize: 16,
+		pxValue: 16,
+		remValue: 1,
+		pxArray: []
+	},
+	created(){
+		for (var i = 1; i <= 40; i++) {
+			this.pxArray.push({ 'px': i })
+		}
 	},
 	computed: {
-		valueRem(){
-			return (this.valuePx / this.fontSize).toFixed(2)
+		columnOne() {
+			return this.pxArray.slice(0, 10)
+		},
+		columnTwo() {
+			return this.pxArray.slice(10, 20)
+		},
+		columnThree() {
+			return this.pxArray.slice(20, 30)
+		},
+		columnFour() {
+			return this.pxArray.slice(30, 40)
+		}
+	},
+	watch : {
+		pxValue: function(val) {
+			this.pxValue = val.toFixed();
+			this.remValue = parseFloat( (val / this.fontSize).toFixed(4) );
+		},
+		remValue: function (val) {
+			this.pxValue = val * this.fontSize;
+			this.remValue = val;
+		},
+		fontSize: function(val) {
+			this.fontSize = val.toFixed();
+			this.pxValue = this.remValue * val;
 		}
 	}
 })
